47c3c9f444ad0a00e91913b26fe79fdf41d7ac02
Maciej Lotkowski <maciej.lotkowski@zenbe.com>
2009-11-01 16:58:22 -0500

    added request methods substitution

--- lib/rack/contrib/superlogger.rb
+++ lib/rack/contrib/superlogger.rb
@@ -1,30 +1,35 @@
 module Rack
   class Superlogger
-    
+    REQUEST_METHODS = Rack::Request.public_instance_methods(false).reject { |method_name| method_name =~ /[=\[]/ }.freeze
+
     def initialize(app, logger, template)
       @app = app
       @logger = logger
       @template = template
     end
-    
+
+     
     def call(env)
       env["rack.superlogger"] = {}
-      before = Time.now.to_f
-
-      status, headers, body = @app.call(env)
       
-      duration = (Time.now.to_f - before.to_f).floor 
+      before = Time.now.to_f
+      status, headers, body = @app.call(env)
+      duration = ((Time.now.to_f - before.to_f) * 1000).floor 
+      
       message = @template.dup
+      request = Rack::Request.new(env)
+      
+      env["rack.superlogger"][:duration] = duration.to_s if message.include? ":duration"
+      env["rack.superlogger"][:status]   = status.to_s   if message.include? ":status"
+      
+      REQUEST_METHODS.each do |method_name|
+        env["rack.superlogger"][method_name.to_sym] = request.send(method_name.to_sym) if message.include?(":#{method_name}")
+      end
       
       env["rack.superlogger"].each do |k, v|
-        message.gsub! ":#{k}", v
+        message.gsub! ":#{k}", v.to_s
       end
 
-      if message =~ /:duration/
-        message.gsub! ":duration", duration.to_s
-      end
-        
-      
       @logger.info message
       [status, headers, body]
     end
--- test/spec_rack_superlogger.rb
+++ test/spec_rack_superlogger.rb
@@ -12,7 +12,7 @@ context "Rack::Superlogger" do
       [200, { "Content-type" => "test/plain", "Content-length" => "3" }, ["foo"]] 
     }
     
-    Rack::Superlogger.new(app, logger, template).call(Rack::MockRequest.env_for("/"))
+    Rack::Superlogger.new(app, logger, template).call(Rack::MockRequest.env_for("?super=logger"))
   end
   
   specify "should substitute :keys in template with values from 'rack.logger'" do
@@ -22,12 +22,26 @@ context "Rack::Superlogger" do
     test_response(logger, ':some_var :something_else')
   end
   
-  specify "should additionally substitute :duration with the runtime of the application" do
+  specify "should substitute :duration with the runtime of the application" do
     logger = mock("logger")
     logger.expects(:info).with("foobar kiszonka 0 ms")
     
     Time.expects(:now).twice.returns(123)
     
     test_response(logger, ':some_var :something_else :duration ms')
+  end
+  
+  specify "should substitute :status with status" do
+    logger = mock("logger")
+    logger.expects(:info).with("foobar kiszonka 200")
+    
+    test_response(logger, ":some_var :something_else :status")
+  end
+  
+  specify "should substitute a method name form Rack::Request with its result" do
+    logger = mock("logger")
+    logger.expects(:info).with("foobar kiszonka  super=logger") #double space - content_type should be nil
+    
+    test_response(logger, ":some_var :something_else :content_type :query_string")
   end
 end
