2ad83602781bba21ffd025124a635e5bc0faade3
Maciej Lotkowski <maciej.lotkowski@zenbe.com>
2009-12-05 16:54:29 -0500

    Further speed improvements - String#% seems to be the fastes way to format log message

--- lib/rack/contrib/superlogger.rb
+++ lib/rack/contrib/superlogger.rb
@@ -27,29 +27,30 @@ module Rack
       class Templated < Base
         def initialize(options)
           @logger   = options.delete(:logger)   or raise ArgumentError, "You must specify a logger"
-          @template = options.delete(:template) or raise ArgumentError, "You must specify a template"
+          @keys = []
+          # extract keys to be substituted and replace them with %s for String#% sybstitution
+          @template = options.delete(:template).gsub(/:\w+/) { |m| @keys << m[1..-1].to_sym; "%s" } or
+            raise ArgumentError, "You must specify a template"
+          @request_methods = @keys & Rack::Superlogger::REQUEST_METHODS
           super options
         end
         
         def process(env)
-          request = Rack::Request.new(env)
-          message = @template.dup
-
-          Rack::Superlogger::REQUEST_METHODS.each do |method_name|
-            env["rack.superlogger.data"][method_name.to_sym] = request.send(method_name.to_sym) if message.include?(":#{method_name}")
+          if @request_methods.any?
+            request = Rack::Request.new(env)
+            @request_methods.each { |m| env["rack.superlogger.data"][m] = request.send(m) }
           end
 
-          env["rack.superlogger.data"].each do |k, v|
-            message[":#{k}"] = v.to_s if message.include?(":#{k}")
-          end
-
-          @logger.info message
+          values = []
+          @keys.each { |k| values << env["rack.superlogger.data"][k.to_sym] }
+           
+          @logger.info @template % values
         end
       end
     end
       
     REQUEST_METHODS = Rack::Request.public_instance_methods(false).
-                        reject { |method_name| method_name =~ /[=\[]|content_length/ }.freeze
+                        reject { |method_name| method_name =~ /[=\[]|content_length/ }.map { |m| m.to_sym }.freeze
 
     def initialize(app, options)
       @app, @processor = app, LogProcessor[options.delete(:type)].new(options)
